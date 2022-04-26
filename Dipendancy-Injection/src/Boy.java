3385a871c88063b297e9ec548b5b2c9abea6e91f
sanuv9683 <sanuv9683@gmail.com>
2022-04-26 11:36:40 +0530

    Content Updatefd

--- Dipendancy-Injection/src/Boy.java
+++ Dipendancy-Injection/src/Boy.java
@@ -2,25 +2,39 @@
  * @author : Sanu Vithanage
  * @since : 0.1.0
  **/
-public class Boy {
+public class Boy implements DI {
 
     //Dependency Injection.. // How to inject dependencies to a class
     //property injection
+    // we can use this, if we know the dependency before creating the Boy object
     //Agreement agreement= new Girl();
 
     Agreement agreement;
 
     //Constructor injection
+    //We can use this, if we know the dependency at the time we create the Boy object
   /*  public Boy(Agreement a) {
         this.agreement = a;
     }*/
 
+    public static void main(String[] args) {
+       /* Boy boy = new Boy(new Girl());
+        boy.chatWithGirl();*/
 
-    //Setter method injection
-    public void setInjection(Agreement a){
-        this.agreement=a;
+   /*     Boy boy = new Boy();
+        boy.setInjection(new Girl());
+        boy.chatWithGirl();*/
+
+        Boy boy = new Boy();
+
+        boy.chatWithGirl();
     }
 
+    //Setter method injection
+    // We can use this, if we know the dependency, just after creating the object
+    public void setInjection(Agreement a) {
+        this.agreement = a;
+    }
 
     public void chatWithGirl() {
         //Loos coupling
@@ -31,13 +45,10 @@ public class Boy {
         agreement.chat();
     }
 
-
-    public static void main(String[] args) {
-       /* Boy boy = new Boy(new Girl());
-        boy.chatWithGirl();*/
-
-        Boy boy = new Boy();
-        boy.setInjection(new Girl());
-        boy.chatWithGirl();
+    @Override
+    public void inject(Agreement a) {
+        agreement = a;
     }
+
+
 }
---
+++ Dipendancy-Injection/src/DI.java
@@ -0,0 +1,7 @@
+/**
+ * @author : Sanu Vithanage
+ * @since : 0.1.0
+ **/
+public interface DI {
+    void inject(Agreement a);
+}
