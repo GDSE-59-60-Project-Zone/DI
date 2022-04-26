81a944165068822677d6706a8f77758bcbaae9b5
sanuv9683 <sanuv9683@gmail.com>
2022-04-26 10:57:43 +0530

    Content Updated

--- Dipendancy-Injection/src/Boy.java
+++ Dipendancy-Injection/src/Boy.java
@@ -4,27 +4,31 @@
  **/
 public class Boy {
 
+    //Dependency Injection.. // How to inject dependencies to a class
     //property injection
-    Agreement agreement= new Girl();
+//    Agreement agreement= new Girl();
 
+    Agreement agreement;
 
+    public Boy(Agreement a) {
+        this.agreement = a;
+    }
 
-    public void chatWithGirl(){
+    public Boy() {
+    }
 
-//        //Tight coupling
-//        Girl girl = new Girl();
-//        girl.chat();
-
-
+    public void chatWithGirl() {
         //Loos coupling
-
         agreement.chat();
     }
 
-    public void test(){
+    public void test() {
         agreement.chat();
     }
 
 
-
+    public static void main(String[] args) {
+        Boy boy = new Boy();
+        boy.chatWithGirl();
+    }
 }
