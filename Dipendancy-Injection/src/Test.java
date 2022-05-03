0f3b0ae40144ca3f5b8e0466e8c1f961838182bb
sanuv9683 <sanuv9683@gmail.com>
2022-05-03 10:30:58 +0530

    Comments updatefd

--- Dipendancy-Injection/src/Test.java
+++ Dipendancy-Injection/src/Test.java
@@ -9,7 +9,7 @@ public class Test {
         test("Insert into Customer values(?,?,?,?)",10,"",true);
     }
 
-    public static void test(String sql,Object... ob){
+    public static void test(String sql,Object... ob){ // we can set optional params after sql by using var args
         System.out.println(sql);
         for (Object s : ob) {
             System.out.println(s);
